270ee29a4d5e0d6a3b63fb0f14a1cfb6b308bbad
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2022-05-04 11:20:18 -0600

    EADME.md

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This is a large project in which a rest server is implemented that allows all kinds of requests, including from JWT validations to google login handling.
 
+## Dependencies and technologies used
++ Express
++ Dotenv
++ Colors
++ CORS
++ 
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
