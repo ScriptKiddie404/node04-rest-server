9dce7f1f506666c4449c498239cc7f53d42a9526
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2022-05-11 11:21:04 -0600

    Algunos cambios en el muddleware de validaciones.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { login } = require('../controllers/auth');
+const { validateFields } = require('../middleware/validate-fields');
 
 const router = Router();
 
-router.post('/login', login)
+router.post('/login', [
+    check('email', 'El correo debe tener un formato válido.').isEmail(),
+    check('email', 'El correo no puede estar vacío.').not().isEmpty(),
+    check('password', 'La contraseña es un campo obligatorio.').not().isEmpty(),
+    validateFields
+], login)
 
 module.exports = router;
